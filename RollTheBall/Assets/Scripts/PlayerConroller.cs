ac0dcf15a6e7d96a24be4225bf75167557d04c8f
Nikita Sergejevs <78017695+Nikita-Sergejevs@users.noreply.github.com>
2022-05-12 19:34:54 +0300

    movement

---
+++ RollTheBall/Assets/Scripts/PlayerConroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConroller : MonoBehaviour
+{
+    public float speed;
+    Rigidbody playerRb;
+
+    void Start()
+    {
+        playerRb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        playerRb.AddForce (move * speed * Time.deltaTime);  
+    }
+}
