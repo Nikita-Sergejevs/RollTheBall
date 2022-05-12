aa5c016b3b7ac690e7a682eac017f0368785bce1
Nikita Sergejevs <78017695+Nikita-Sergejevs@users.noreply.github.com>
2022-05-12 19:59:07 +0300

    Cam

---
+++ RollTheBall/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject playerObject;
+    private Vector3 camOffset;
+
+    public void Start()
+    {
+        camOffset = this.transform.position - playerObject.transform.position;
+    }
+
+    public void LateUpdate()
+    {
+        this.transform.position = playerObject.transform.position + camOffset;
+    }
+}
