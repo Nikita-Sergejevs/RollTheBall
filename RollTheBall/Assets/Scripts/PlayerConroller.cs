dabf139927de42c8a489b989939bd1f119951b3d
Nikita Sergejevs <78017695+Nikita-Sergejevs@users.noreply.github.com>
2022-05-12 22:26:40 +0300

    Speed Bonuse

--- RollTheBall/Assets/Scripts/PlayerConroller.cs
+++ RollTheBall/Assets/Scripts/PlayerConroller.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 
 public class PlayerConroller : MonoBehaviour
 {
-    public float speed;
     public Text scoreText;
-
+    private float speed;
     private Rigidbody playerRb;
     private int count;
     private GameObject[] games;
     private Vector3 startposition;
+    private float boostTimer;
+    private bool boosting;
 
     void Start()
     {
@@ -20,6 +21,9 @@ public class PlayerConroller : MonoBehaviour
         startposition = playerRb.position;
         count = 0;
         UpdateScoreText();
+        boostTimer = 0;
+        boosting = false;
+        speed = 1000;
     }
 
     void Update()
@@ -30,6 +34,17 @@ public class PlayerConroller : MonoBehaviour
         Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         playerRb.AddForce(move * speed * Time.deltaTime);  
+
+        if(boosting)
+        {
+            boostTimer += Time.deltaTime;
+            if (boostTimer >= 5)
+            {
+                speed = 1000;
+                boostTimer = 0;
+                boosting = false;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider collider)
@@ -40,11 +55,15 @@ public class PlayerConroller : MonoBehaviour
             count++;
             UpdateScoreText();
         }
-        else if(collider.gameObject.CompareTag("Win"))
-            restartGameState();
         else if(collider.gameObject.CompareTag("Spikes"))
         {
             restartGameState();
+        }
+        else if(collider.gameObject.CompareTag("Bonuse"))
+        { 
+            boosting = true;
+            speed = 2000;
+            collider.gameObject.SetActive(false);
         }
     }
 
---
+++ RollTheBall/Assets/Scripts/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+    private Vector3 rotation;
+
+    public void Start()
+    {
+        rotation = new Vector3(0,90);
+    }
+
+    public void Update()
+    {
+        this.transform.Rotate(rotation * speed * Time.deltaTime);
+    }
+}
