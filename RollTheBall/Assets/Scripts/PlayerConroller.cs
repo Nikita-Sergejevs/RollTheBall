0a9d8253772befdacfa1001388b62ecd2ab7f138
Nikita Sergejevs <78017695+Nikita-Sergejevs@users.noreply.github.com>
2022-05-16 01:33:24 +0300

    Stars and Finish

--- RollTheBall/Assets/Scripts/PlayerConroller.cs
+++ RollTheBall/Assets/Scripts/PlayerConroller.cs
@@ -6,23 +6,44 @@ using UnityEngine.UI;
 public class PlayerConroller : MonoBehaviour
 {
     public Text scoreText;
-    private float speed;
+    public Text StarText;
+    public Text WinText;
+    public Button playAgainButton;
+
     private Rigidbody playerRb;
-    private int count;
-    private int starsCount;
-    private GameObject[] games;
+
+    public GameObject UiObject;
+    public GameObject Finish;
+    private GameObject[] gems;
+    private GameObject[] stars;
+
     private Vector3 startposition;
+
     private float boostTimer;
+    private float speed;
+
     private bool boosting;
+    private bool gameStarted;
+
+    private int count;
+    private int countstars;
 
     void Start()
     {
         playerRb = this.GetComponent<Rigidbody>();
-        games = GameObject.FindGameObjectsWithTag("Spikes");
+        gems = GameObject.FindGameObjectsWithTag("Gem");
+        stars = GameObject.FindGameObjectsWithTag("Star");
         startposition = playerRb.position;
+        playAgainButton.onClick.AddListener(playAgainButtonAction);
+        gameStarted = true;
         count = 0;
-        starsCount = 0;
+        countstars = 0;
         UpdateScoreText();
+        UpdateStarText();
+        UiObject.SetActive(false);
+        Finish.SetActive(false);
+        WinText.gameObject.SetActive(false);
+        playAgainButton.gameObject.SetActive(false);
         boostTimer = 0;
         boosting = false;
         speed = 1000;
@@ -30,21 +51,24 @@ public class PlayerConroller : MonoBehaviour
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        playerRb.AddForce(move * speed * Time.deltaTime);  
-
-        if(boosting)
+        if (gameStarted)
         {
-            boostTimer += Time.deltaTime;
-            if (boostTimer >= 5)
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+            playerRb.AddForce(move * speed * Time.deltaTime);
+
+            if (boosting)
             {
-                speed = 1000;
-                boostTimer = 0;
-                boosting = false;
+                boostTimer += Time.deltaTime;
+                if (boostTimer >= 5)
+                {
+                    speed = 1000;
+                    boostTimer = 0;
+                    boosting = false;
+                }
             }
         }
     }
@@ -57,9 +81,15 @@ public class PlayerConroller : MonoBehaviour
             count++;
             UpdateScoreText();
         }
-        else if(collider.gameObject.CompareTag("Spikes"))
+        else if(collider.gameObject.CompareTag("Win"))
         {
+            gameStarted = false;
             restartGameState();
+            WinText.text = "Ты выйграл!\n Твой счет: " + count;
+            WinText.gameObject.SetActive(true);
+            playAgainButton.gameObject.SetActive(true);
+            scoreText.gameObject.SetActive(false);
+
         }
         else if(collider.gameObject.CompareTag("Bonuse"))
         { 
@@ -67,23 +97,56 @@ public class PlayerConroller : MonoBehaviour
             speed = 2000;
             collider.gameObject.SetActive(false);
         }
+        
+        if(collider.gameObject.CompareTag("Star"))
+        {
+            collider.gameObject.SetActive(false);
+            countstars++;
+            UpdateStarText();
+        }
+        else if(countstars == 3)
+        {
+            UiObject.SetActive(true);
+            Finish.SetActive(true);
+        }
     }
 
     void UpdateScoreText()
     {
         scoreText.text = "Твой счет: " + count;
     }
+    void UpdateStarText()
+    {
+        StarText.text = "Звезд: " + countstars;
+    }
 
-    void restartGameState()
+    void playAgainButtonAction()
     {
         count = 0;
+        countstars = 0;
         UpdateScoreText();
+        UpdateStarText();
+        UiObject.SetActive(false);
+        Finish.SetActive(false);
+        scoreText.gameObject.SetActive(true);
+        WinText.gameObject.SetActive(false);
+        playAgainButton.gameObject.SetActive(false);
+        gameStarted = true;
+    }
 
-        for(int i = 0; i < games.Length; i++)
+    void restartGameState()
+    { 
+        for(int i = 0; i < gems.Length; i++)
         {
-            games[i].gameObject.SetActive(true);
+            gems[i].gameObject.SetActive(true);
         }
 
+        for(int i = 0;i < stars.Length; i++)
+        {
+            stars[i].gameObject.SetActive(true);
+        }
+
+        Finish.SetActive(false);
         playerRb.position = startposition;
         playerRb.velocity = Vector3.zero;
         playerRb.angularVelocity = Vector3.zero;
